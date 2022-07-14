b692055c3a0a9727ef79c6672662b8beb5a395bc
Chuston13 <cdhprogrammer@gmail.com>
2022-07-14 14:33:39 -0500

    index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary=[];
+
+function Book(){
+    this.author = author
+    this.title =  title
+    this.read = read
+    this.info = function (){
+
+    
+    return ++ this.author;        
+
+
+}}
+function addBook(){
+    addBook == myLibrary;
+    prompt("hello")
+}
